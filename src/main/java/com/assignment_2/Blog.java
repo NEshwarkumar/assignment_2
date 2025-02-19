510dcdbaab1eaf29fd0535adac9d73ad6995228e
NEshwar Kumar <eshwar961@gmail.com>
2025-02-18 22:48:24 -0500

    Blog.java: Added the getPostsByAuthorAge method. BlogTest.java: Added test cases for getPostsByAuthorAge. person.json and blogPosts.json: Used for data-driven testing.

--- src/main/java/com/assignment_2/Blog.java
+++ src/main/java/com/assignment_2/Blog.java
@@ -2,7 +2,10 @@ package com.assignment_2;
 
 import lombok.Getter;
 import lombok.Setter;
+
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 @Getter
 @Setter
@@ -10,6 +13,23 @@ public class Blog {
 
     private List<BlogPost> posts;
     private List<Person> contributors;
+
+    public List<String> getPostsByAuthorAge(Integer age) {
+        return posts.stream()
+                .filter(post -> contributors.stream()
+                        .filter(person -> person.getAge().equals(age))
+                        .anyMatch(person -> person.getId().equals(post.getAuthorId())))
+                .map(BlogPost::getId)
+                .collect(Collectors.toList());
+    }
+
+
+    private Person findAuthorById(String authorId) {
+        return contributors.stream()
+                .filter(person -> Objects.equals(person.getId(), authorId))
+                .findFirst()
+                .orElse(null);
+    }
 
     @Override
     public String toString() {
@@ -21,11 +41,11 @@ public class Blog {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Blog blog = (Blog) o;
-        return posts.equals(blog.posts) && contributors.equals(blog.contributors);
+        return Objects.equals(posts, blog.posts) && Objects.equals(contributors, blog.contributors);
     }
 
     @Override
     public int hashCode() {
-        return posts.hashCode() + contributors.hashCode();
+        return Objects.hash(posts, contributors);
     }
 }
